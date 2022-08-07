13bb10c23c893bdf5ebcef1ae9a304bb212ce0c3
webmfl <66096850+webmfl@users.noreply.github.com>
2022-08-07 07:55:42 -0300

    Update index.php
    base

--- index.php
+++ index.php
@@ -1,13 +1,92 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Portfolio Martín Lazo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container" id="top">
+      <!-- Barra Nav principal -->
+      <header>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="https://linkedin.com/in/martinlazo">Martín Lazo</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#top">Inicio</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Proyectos</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="https://inep.com.ar" target="destino">INEP</a></li>
+                  <li><a class="dropdown-item" href="https://bytes.inep.com.ar" target="destino">Bytes</a></li>
+                </ul>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link href="contacto.php">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      </header>
+
+      <section>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="h-100 p-5 text-bg-dark rounded-3">
+              <h2>¡Bienvenidos!</h2>
+              <p>Este es un primer intento de maquetación para el desafío de la segunda unidad utilizando Bootstrap (y otros elementos que podemos obtener del sitio web --como este Jumbotron--)</p>
+              <p>También agregué otros ejemplos de HTML explicados en el modulo para definir otros detalles. A medida de ir superando unidades, este portfolio cambiará sustancialmente...</p>
+             
+            </div>
+          </div>
+        </div>
+      </section>
+
+      
+        <section style="margin-top: 20px;">
+          <div class="row align-items-center">">
+            <div class="col-md-4">
+              <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+                  
+        </section>
+      </div>
+    </div>
+
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
